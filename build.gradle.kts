f761353278753697be26f4df80549d26d38b7a7e
rrdtlsh <144782829+rrdtlsh@users.noreply.github.com>
2025-06-12 16:10:38 +0700

    Delete build.gradle.kts

--- build.gradle.kts
+++
@@ -1,7 +0,0 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    kotlin("plugin.serialization") version "1.9.0"
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
-    alias(libs.plugins.kotlin.compose) apply false
-}
